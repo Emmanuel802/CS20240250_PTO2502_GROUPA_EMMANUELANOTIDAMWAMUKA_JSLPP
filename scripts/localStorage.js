a8b5b5facdc2bbe5421ba550645e81a92b010ba9
Emmanuel Mwamuka <a.emmanuelll.17@gmail.com>
2025-08-15 18:17:24 +0200

    Added a function that loads tasks from localStorage

--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -1,2 +1,13 @@
 // A constant to store the key used in localStorage
 const STORAGE_KEY = "tasks";
+
+/**
+ * Loads tasks from localStorage.
+ *
+ * @returns {Array<Object>|null} Returns an array of task objects if found, otherwise null.
+ *
+ */
+export function loadTasks() {
+  const stored = localStorage.getItem(STORAGE_KEY);
+  return stored ? JSON.parse(stored) : null;
+}
