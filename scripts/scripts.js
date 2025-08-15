2c1ac2fdf28bc53c1b3fe194d5b4ced5ccd3cf69
Emmanuel Mwamuka <a.emmanuelll.17@gmail.com>
2025-08-15 18:49:07 +0200

    Set tasks from localStorage or default data

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,10 @@
 import { initialData } from "./initialData.js";
 import { saveTasks, loadTasks } from "./localStorage.js";
+
+/**
+ * @type {Array<Object>} tasks
+ * This holds the current list of tasks in memory.
+ * It first tries to load from localStorage;
+ * if none exist, it falls back to the initialData array.
+ */
+let tasks = loadTasks() || initialData;
