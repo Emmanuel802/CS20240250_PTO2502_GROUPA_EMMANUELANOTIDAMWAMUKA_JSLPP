7d17aa60bca6c563cce9070d3aa3458c7b84e9f3
Emmanuel <a.emmanuelll.17@gmail.com>
2025-09-19 17:20:42 +0200

    Set tasks from localStorage or default data

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,11 @@
 import { initialData } from "./initialData.js";
 import { saveTasks, loadTasks } from "./localStorage.js";
 import { openEditModal, openNewTaskModal } from "./modal.js";
+
+/**
+ * @type {Array<Object>} tasks
+ * This holds the current list of tasks in memory.
+ * It first tries to load from localStorage;
+ * if none exist, it falls back to the initialData array.
+ */
+let tasks = loadTasks() || initialData;
