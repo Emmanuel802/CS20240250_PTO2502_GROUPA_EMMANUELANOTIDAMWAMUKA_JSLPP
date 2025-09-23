06fffa01516b26354b72447917831c54cfdee09e
Emmanuel <a.emmanuelll.17@gmail.com>
2025-09-23 07:38:14 +0200

    add DOM element references for sidebar, mobile menu, and theme toggle

---
+++ scripts/sideBar.js
@@ -0,0 +1,13 @@
+// ======================
+// DOM Elements
+// ======================
+const sidebar = document.getElementById("side-bar-div");
+const hideSidebarBtn = document.getElementById("hide-sidebar-btn");
+const showSidebarBtn = document.getElementById("show-sidebar-btn");
+
+const mobileMenu = document.getElementById("mobile-menu");
+const logoMobile = document.getElementById("logo-mobile");
+const closeMobileMenuBtn = document.getElementById("close-mobile-menu");
+
+const themeSwitch = document.getElementById("theme-switch");
+const mobileThemeSwitch = document.getElementById("mobile-theme-switch");
